3d14f96b1c560e481921deb91dc312b8cd75856f
fida1989 <fida_muntaseer@yahoo.com>
2015-01-18 15:55:07 +0600

    2nd commit

--- src/com/example/hw_project/MainActivity.java
+++ src/com/example/hw_project/MainActivity.java
@@ -1,42 +1,77 @@
 package com.example.hw_project;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
-
-
-
-
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-	
+public class MainActivity extends Activity {
+
 	Button register;
+	Button login;
+	Button logout;
+	EditText email, pass;
+	String user_email, user_pass;
+	String result;
+	String jsonresult;
+	String jsonstring;
+	String json;
+	String send_jsonstring;
+	ProgressDialog mProgress;
+	LinearLayout lin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mProgress = new ProgressDialog(MainActivity.this);
 		initializeComponent();
+
 	}
-	
-	
 
 	private void initializeComponent() {
 		// TODO Auto-generated method stub
+		email = (EditText) findViewById(R.id.editloginEmail);
+		pass = (EditText) findViewById(R.id.editloginPass);
+		login = (Button) findViewById(R.id.loginoption);
+		// logout = (Button) findViewById(R.id.logout);
+		login.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				getFormData();
+			}
+		});
 		register = (Button) findViewById(R.id.registeroption);
 		register.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				//register.setBackgroundColor(Color.CYAN);
+				// register.setBackgroundColor(Color.CYAN);
 
 				Intent i = new Intent(getApplicationContext(),
 						RegisterActivity.class);
@@ -45,27 +80,124 @@ public class MainActivity extends ActionBarActivity {
 			}
 		});
 
-		
 	}
 
+	public class RegisterTask extends AsyncTask<String, String, String> {
 
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			mProgress.dismiss();
+			try {
+				JSONObject jsonResult = new JSONObject(result);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+				System.out.println("Return JSON Object: "
+						+ jsonResult.toString());
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+				String msg = jsonResult.getString("message");
+
+				if (msg.equals("success")) {
+
+					JSONObject user = jsonResult.getJSONObject("details");
+					System.out.println("Success JSON: " + user.toString());
+					// System.out.println("User ID: " + user.getString("id"));
+
+					SharedPreferences pref = getApplicationContext()
+							.getSharedPreferences("MyPref", 0); // 0 - for
+																// private mode
+					Editor editor = pref.edit();
+					editor.putString("user_id", user.getString("id"));
+					// Storing
+					editor.putBoolean("log_track", true); // string
+					editor.commit(); // commit changes
+
+					Toast.makeText(getApplicationContext(),
+							"Login Successful!", Toast.LENGTH_SHORT).show();
+
+					/*
+					 * Intent i = new Intent(LoginActivity.this,
+					 * MainActivity.class); startActivity(i);
+					 */
+					//finish();
+					email.setText("");
+					pass.setText("");
+
+				}
+
+				else {
+
+					Toast.makeText(getApplicationContext(),
+							"Login Failure! Check username/password!",
+							Toast.LENGTH_LONG).show();
+
+				}
+
+			} catch (Exception e) {
+
+				Toast.makeText(getApplicationContext(), "Error!",
+						Toast.LENGTH_SHORT).show();
+				System.out.println("JSON Parse Error: " + result);
+			}
 		}
-		return super.onOptionsItemSelected(item);
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mProgress.setMessage("Loging...");
+			mProgress.show();
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			// name, email, phone, password
+			/*
+			 * json = createJSON(user_name, user_email, user_password,
+			 * user_phone);
+			 */
+
+			try {
+
+				// System.out.println(send_jsonstring);
+
+				List<NameValuePair> reg_data = new ArrayList<NameValuePair>();
+				reg_data.add(new BasicNameValuePair("email", user_email));
+
+				reg_data.add(new BasicNameValuePair("password", user_pass));
+
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpPost httppost = new HttpPost(
+						"http://mobioapp.net/apps/bdcyclists/public/login_json");
+				httppost.setEntity(new UrlEncodedFormEntity(reg_data));
+				HttpResponse response = httpclient.execute(httppost);
+				HttpEntity entity = response.getEntity();
+				String htmlResponse = EntityUtils.toString(entity);
+				result = htmlResponse;
+				System.out.println("JSON Try: " + result);
+
+			} catch (Exception e) {
+				result = e.toString();
+				System.out.println("JSON Catch: " + result);
+
+			}
+
+			return result;
+		}
+
 	}
+
+	public void getFormData() {
+		// name, email, phone, password
+		user_email = email.getText().toString();
+		user_pass = pass.getText().toString();
+
+		if (user_email.length() > 0 && user_pass.length() > 0) {
+
+			new RegisterTask().execute();
+		} else {
+
+			Toast.makeText(getApplicationContext(), "Fill Required Info!",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
 }
--- src/com/example/hw_project/RegisterActivity.java
+++ src/com/example/hw_project/RegisterActivity.java
@@ -1,14 +1,224 @@
 package com.example.hw_project;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//import org.apache.james.mime4j.stream.NameValuePair;
 
 public class RegisterActivity extends Activity {
+
+	Button submit;
+	EditText name, desc, location, fb, web, email, phone, password, image1;
+	String user_name, user_desc, user_location, user_fb, user_web, user_email,
+			user_phone, user_password, user_image;
+	String result;
+	String jsonresult;
+	String jsonstring;
+	String json;
+	String send_jsonstring;
+	ProgressDialog mProgress;
+	String user;
+	List<NameValuePair> nameValuePairs;
+	boolean server;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
+		mProgress = new ProgressDialog(RegisterActivity.this);
+		initializeComponent();
 
 	}
+
+	private void initializeComponent() {
+		name = (EditText) findViewById(R.id.editgName);
+
+		location = (EditText) findViewById(R.id.editglocation);
+
+		email = (EditText) findViewById(R.id.editgemail);
+		phone = (EditText) findViewById(R.id.editgphone);
+		password = (EditText) findViewById(R.id.editgpass);
+		submit = (Button) findViewById(R.id.gsubmit);
+		submit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				getFormData();
+
+			}
+		});
+
+	}
+
+	public class RegisterTask extends AsyncTask<String, String, String> {
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			mProgress.dismiss();
+			try {
+				JSONObject jsonResult = new JSONObject(result);
+
+				System.out.println("Return JSON Object: "
+						+ jsonResult.toString());
+
+				String msg = jsonResult.getString("message");
+
+				if (msg.equals("success")) {
+
+					JSONObject user = jsonResult.getJSONObject("details");
+					System.out.println("Success JSON: " + user.toString());
+
+					Toast.makeText(getApplicationContext(),
+							"Registration Successful!", Toast.LENGTH_SHORT)
+							.show();
+
+					final Intent intent = new Intent(RegisterActivity.this,
+							MainActivity.class);
+
+					Thread thread = new Thread() {
+						@Override
+						public void run() {
+							try {
+								Thread.sleep(2000); // As I am using LENGTH_LONG
+													// in Toast
+								startActivity(intent);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					};
+					thread.start();
+
+				}
+
+				else {
+
+					Toast.makeText(getApplicationContext(),
+							"Registration Failure!", Toast.LENGTH_SHORT).show();
+
+				}
+
+			} catch (Exception e) {
+
+				Toast.makeText(getApplicationContext(), "Error!",
+						Toast.LENGTH_SHORT).show();
+				System.out.println("JSON Parse Error: " + result);
+			}
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mProgress.setMessage("Registering...");
+			mProgress.show();
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+
+			try {
+
+				List<NameValuePair> reg_data = new ArrayList<NameValuePair>();
+				reg_data.add(new BasicNameValuePair("name", user_name));
+				reg_data.add(new BasicNameValuePair("desc", user_desc));
+				reg_data.add(new BasicNameValuePair("location", user_location));
+				reg_data.add(new BasicNameValuePair("email", user_email));
+				reg_data.add(new BasicNameValuePair("password", user_password));
+				reg_data.add(new BasicNameValuePair("phone", user_phone));
+
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpPost httppost = new HttpPost(
+						"http://mobioapp.net/apps/bdcyclists/public/group_signup_json");
+				httppost.setEntity(new UrlEncodedFormEntity(reg_data));
+				HttpResponse response = httpclient.execute(httppost);
+				HttpEntity entity = response.getEntity();
+				String htmlResponse = EntityUtils.toString(entity);
+				result = htmlResponse;
+				System.out.println("JSON Try: " + result);
+
+			} catch (Exception e) {
+				result = e.toString();
+				System.out.println("JSON Catch: " + result);
+
+			}
+
+			return result;
+		}
+
+	}
+
+	public void getFormData() {
+
+		user_name = name.getText().toString();
+		user_location = location.getText().toString();
+		user_email = email.getText().toString();
+		user_phone = phone.getText().toString();
+		user_password = password.getText().toString();
+
+		if (user_name.length() > 0 && user_location.length() > 0
+				&& user_email.length() > 0 && user_password.length() > 0
+				&& user_phone.length() > 0) {
+
+			// String email1 = email.getText().toString();
+			if (!isValidEmail(user_email)) {
+				email.setError("Invalid Email");
+				return;
+			}
+
+			// String pass1 = password.getText().toString();
+			if (!isValidPassword(user_password)) {
+				password.setError("Password length must be 6 digits");
+				return;
+			}
+
+			new RegisterTask().execute();
+		} else {
+
+			Toast.makeText(getApplicationContext(), "Fill Required Info!",
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	private boolean isValidPassword(String pass1) {
+		if (pass1 != null && pass1.length() > 5) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isValidEmail(String email1) {
+		String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		Matcher matcher = pattern.matcher(email1);
+		return matcher.matches();
+	}
+
 }
